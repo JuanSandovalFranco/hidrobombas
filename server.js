ea7b062228f6c5b225de39b063cbb07bfcdc71d3
juan <juansandovalfranco21@gmail.com>
2022-07-14 11:47:27 -0500

    web

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = process.env.PORT || 10000;
 
-app.listen(8000, () => {
+app.listen(PORT, () => {
   console.log("listen on port " + PORT);
 });
