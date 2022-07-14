c4a2aaa7c04fdca3f0dd37e6a8e5e44fbc1b9111
juan <juansandovalfranco21@gmail.com>
2022-07-14 11:42:59 -0500

    web

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 
+const env = require("dotenv");
+
 const path = require("path");
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
 
-const PORT = 8000;
+const PORT = process.env.PORT || 10000;
 
 app.listen(8000, () => {
   console.log("listen on port " + PORT);
